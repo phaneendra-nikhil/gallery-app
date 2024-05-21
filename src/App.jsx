cb1e04ef2df2ec8e73e83a941f03b7cc778fe910
Shanmukh025 <shanmukh2564@gmail.com>
2024-05-21 12:38:22 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <>
-      hello
-    </>
-  )
+    return <>hello Shan</>;
 }
 
-export default App
+export default App;
